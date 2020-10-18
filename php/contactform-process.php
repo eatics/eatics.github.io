ac0cf0c16364e3d04fa6b2d673a93829802a5eaf
Cosmin <petregcosmin@gmail.com>
2020-10-18 17:12:57 +0300

    contact php updated

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -25,8 +25,8 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
-$Subject = "New message from Aria landing page";
+$EmailTo = "eaticsapp@gmail.com";
+$Subject = "New message from" + $name;
 
 // prepare email body text
 $Body = "";
