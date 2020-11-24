e9cf83db3d3824de6d6ba3b2ea56d40a1f3b4a4d
alexandrupeticila <alexandru.peticila@yahoo.com>
2020-11-24 23:27:18 +0200

    add newsletter to landing page

--- js/scripts.js
+++ js/scripts.js
@@ -31,6 +31,32 @@
 		}
     });
 
+    // function for newsletter
+    $(function(){
+        $('.one form .btn').on('click',function(){
+            $(this).parents('.one').animate({
+                top : '-500px'
+            },500);
+
+            $(this).parents('.one').siblings('.two').
+            animate({
+                top : '0px'
+            },500);
+            return false;
+        });
+
+        $('.two .close').on('click',function(){
+            $(this).parent().animate({
+                top : '-500px'
+            },500);
+
+            $(this).parent().siblings('.one').animate({
+                top : '0px'
+            },500);
+            return false;
+        });
+    });
+
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
 		$(document).on('click', 'a.page-scroll', function(event) {
